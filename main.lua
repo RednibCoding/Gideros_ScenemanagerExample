09338472fabb2b9251d287da2ed7ba879dd8e8ac
Michael Binder <binder.mich@gmx.de>
2017-11-09 11:12:52 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,54 @@
+
+--[[
+This code is to demonstrate how to use the scene manager in the Gideros engine
+The "scenemanager.lua" is required
+
+
+by: Michael Binder Nov-2017
+]]
+
+
+-- Create a new scene manager
+local sceneManager = SceneManager.new({
+	["scene1"] = Scene1,
+	["scene2"] = Scene2,
+})
+-- adding it to the stage
+stage:addChild(sceneManager)
+
+-- setting the transition between scenes
+local transition = SceneManager.fade
+
+-- setting scenes table
+local scenes = {"scene1", "scene2"}
+
+
+-- adding buttons to the stage (this is not necassary for the scene manager,
+-- just so that we can switch scenes)
+-- this requires button.lua
+local btnNext = Button.new(Bitmap.new(Texture.new("gfx/btnNext.png")), Bitmap.new(Texture.new("gfx/btnNext_down.png")))
+btnNext:setPosition(200, 100)
+stage:addChild(btnNext)
+
+local btnPrev = Button.new(Bitmap.new(Texture.new("gfx/btnPrev.png")), Bitmap.new(Texture.new("gfx/btnPrev_down.png")))
+btnPrev:setPosition(20, 100)
+stage:addChild(btnPrev)
+
+-- adding an event to the button so it changes scenes when we click on it
+btnNext:addEventListener("click", 
+	function()	
+		sceneManager:changeScene(scenes[2], 1, transition) 
+	end)
+
+btnPrev:addEventListener("click", 
+	function()	
+		sceneManager:changeScene(scenes[1], 1, transition) 
+	end)
+
+
+-- set the first scene to go to
+sceneManager:changeScene(scenes[1])
+	
+	
+	
+	
